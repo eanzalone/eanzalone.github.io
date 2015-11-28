72450e1f9845a981d816ed6a54f193b5a2faf08a
eanzalone <emily.anzalone@gmail.com>
2015-11-27 22:18:28 -0800

    Contact Form and Btns

--- scripts.js
+++ scripts.js
@@ -2,26 +2,11 @@ console.log("Client Side JS Connected.");
 $(document).ready(function(){
 	console.log("Document Ready");
 
-
-
-
-$('#contact-btn').click(function(e){
-	console.log("contact-btn clicked");
-	window.location.replace('file:///Users/emilyanzalone/dev/portfolio/contact.html');
-});
-$('#home-btn').click(function(e){
-	console.log("home-btn clicked");
-	window.location.replace('file:///Users/emilyanzalone/dev/portfolio/home.html');
-});
-
-
 (function(d, t){
    var g = d.createElement(t),
        s = d.getElementsByTagName(t)[0];
-   g.src = "http://www.foxyform.com/js.php?id=725796&sec_hash=ee6c8ca016a&width=350px";
+   g.src = "http://www.foxyform.com/js.php?id=725845&sec_hash=94006383102&width=350px";
    s.parentNode.insertBefore(g, s);
 }(document, "script"));
-
-
 
 }); //close doc ready
