ae56de8c5dcddff2955720bddf0546975efe34ba
eanzalone <emily.anzalone@gmail.com>
2016-01-08 15:58:29 -0800

    Meet and Hire Alert

--- scripts.js
+++ scripts.js
@@ -1,6 +1,11 @@
 console.log("Client Side JS Connected.");
 $(document).ready(function(){
 	console.log("Document Ready");
+	// alert("Hello! I am an alert box!!");
+	if (window.confirm('Interested in meeting Emily? Come to General Assembly\'s Meet & Hire! Wednesday, January 13th.\nClick OK to be redirected to the event page. Click Cancel to continue browsing this page.')) 
+	{
+	window.location.href='https://generalassemb.ly/education/web-developer-meet-greet-ef751d9f-4e50-4bc5-9d60-e231c2992d15/san-francisco/17130';
+	};
 
 	(function(d, t){
 	   var g = d.createElement(t),
