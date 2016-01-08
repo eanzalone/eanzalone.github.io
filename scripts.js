aefcf6fb747524e0f984ef4121a566197a0f982e
eanzalone <emily.anzalone@gmail.com>
2016-01-08 15:45:08 -0800

    Friday edits

--- scripts.js
+++ scripts.js
@@ -2,11 +2,11 @@ console.log("Client Side JS Connected.");
 $(document).ready(function(){
 	console.log("Document Ready");
 
-(function(d, t){
-   var g = d.createElement(t),
-       s = d.getElementsByTagName(t)[0];
-   g.src = "http://www.foxyform.com/js.php?id=725845&sec_hash=94006383102&width=350px";
-   s.parentNode.insertBefore(g, s);
-}(document, "script"));
-
+	(function(d, t){
+	   var g = d.createElement(t),
+	       s = d.getElementsByTagName(t)[0];
+	   g.src = "http://www.foxyform.com/js.php?id=737880&sec_hash=45202e02d88&width=350px";
+	   s.parentNode.insertBefore(g, s);
+	}(document, "script"));
+	
 }); //close doc ready
